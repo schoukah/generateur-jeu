c73e3a2b7a6f9d6e30fb40a2c7ad262d90d54aae
schoukah <21149759+schoukah@users.noreply.github.com>
2021-03-28 13:51:51 -0400

    middle of chapter 4.1 of MERN essential training

--- backend/controllers/playerControllers.js
+++ backend/controllers/playerControllers.js
@@ -27,10 +27,42 @@ export const addNewPlayer = (req, res) => {
 };
 
 export const getPlayers = (req, res) => {
-  Player.find(({}, err, Player) => {
+  Player.find({}, (err, Player) => {
     if (err) {
       res.send(err);
     }
     res.json(Player);
+  });
+};
+
+export const getPlayerWithID = (req, res) => {
+  Player.findById(req.params.PlayerId, (err, Player) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(Player);
+  });
+};
+
+export const UpdatePlayer = (req, res) => {
+  Player.findOneAndUpdate(
+    { _id: req.params.PlayerId },
+    req.body,
+    { new: true, useFindAndModify: false },
+    (err, Player) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(Player);
+    }
+  );
+};
+
+export const deletePlayer = (req, res) => {
+  Player.deleteOne({ _id: req.params.PlayerId }, (err, Player) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: "Successfully deleted player" });
   });
 };
--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -1,11 +1,27 @@
-import { addNewPlayer, getPlayers } from "../controllers/playerControllers";
+import {
+  addNewPlayer,
+  getPlayers,
+  getPlayerWithID,
+  UpdatePlayer,
+  deletePlayer,
+} from "../controllers/playerControllers";
 
 const routes = (app) => {
   app
     .route("/players")
+    // GET endpoint
     .get(getPlayers)
     // POST endpoint
     .post(addNewPlayer);
+
+  app
+    .route("/player/:PlayerId")
+    // get specitific player
+    .get(getPlayerWithID)
+    // update specific Player
+    .put(UpdatePlayer)
+    // update specific Player
+    .delete(deletePlayer);
 };
 
 export default routes;
