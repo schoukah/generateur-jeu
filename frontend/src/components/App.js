0bf2b5d4a87ec5c5d8d799e040cd21efb247c118
schoukah <21149759+schoukah@users.noreply.github.com>
2021-03-29 00:38:29 -0400

    changed game for MERN essential training

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,12 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import ObjectList from "./components/ObjectChoices";
+//import ObjectList from "./components/ObjectChoices";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Bienvenue à la rouelette de jeu</h2>
-        <ObjectList />
       </header>
     </div>
   );
